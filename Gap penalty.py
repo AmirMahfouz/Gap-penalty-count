8c541a39c3ef11d704b5266cd0607bb8abad7544
AmirMahfouz <101039892+AmirMahfouz@users.noreply.github.com>
2023-03-28 20:23:03 +0200

    Add files via upload

---
+++ Gap penalty.py
@@ -0,0 +1,17 @@
+d =  int (input("Enter the Opening Gaps : "))
+n = int (input("Enter the length of Gaps : "))
+e = int (input("Enter the Extending Gaps : ") )
+
+
+Score = int(d + (n-1)*e )
+
+print(Score)
+
+
+
+
+
+
+
+
+input("ENTEr to EXIT ...!")
